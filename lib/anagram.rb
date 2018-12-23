9e144cd051540533603168e0e20d3ced7cebe1ea
Joe Ganobsik <jganobsik@gmail.com>
2018-12-23 14:45:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    
+    
+    def match(input1, test_array)
+    valid_matches = []
+    ana_ary = input1.scan(/\w/).sort
+
+    test_array.each do |testcase|
+        test_scanned = testcase.scan(/\w/).sort 
+        if test_scanned ==  ana_ary
+            valid_matches << test_scanned
+        end
+    end
+
+    return valid_matches
+
+    end
+end
